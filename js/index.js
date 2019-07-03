0615f74d467e0c8bb7409eef82e3cb53b4bc5241
laugoabad <36644178+laugoabad@users.noreply.github.com>
2019-07-03 16:10:46 -0300

    2

--- js/index.js
+++ js/index.js
@@ -1,54 +1,59 @@
 
 jQuery(function($) {
 
-//Al seleccionar el radio de las operaciones, le cambia el backgroundColor
-//a la operacion seleccionada
+  //Al seleccionar la operacion, le cambia el backgroundColor
+  //a la operacion seleccionada
   $('input:radio[name="operacion"]').on("click", function() {
     var radioChecked = document.querySelector('input[name="operacion"]:checked');
-    radioChecked.parentElement.style.backgroundColor = "rgb(195, 29, 36)";
-   });
+    radioChecked.parentElement.style.backgroundColor = "rgb(254, 73, 0)";
+  });
 
 
-//Hace los cálculos con el click en el botón '='
+  //Hace los cálculos con el click en el botón '='
   $('#calcular').click(function() {
-  var operando1 = parseInt(document.getElementById('operando1').value);
-  var operando2 = parseInt(document.getElementById('operando2').value);
-  var radioChecked = document.querySelector('input[name="operacion"]:checked');
-  var operacion = radioChecked.value;
-  var resultado;
-  switch (operacion) {
-    case "+" : {
-      resultado = operando1 + operando2;
-      break;
+    var operando1 = parseInt(document.getElementById('operando1').value);
+    var operando2 = parseInt(document.getElementById('operando2').value);
+    var radioChecked = document.querySelector('input[name="operacion"]:checked');
+    var operacion = radioChecked.value;
+    var resultado;
+    if ((isNaN(operando1)) || (isNaN(operando2))) {
+      alert('Los valores ingresados no son correctos');
+      return;
     }
-    case "-" : {
-      resultado = operando1 - operando2;
-      break;
+
+    switch (operacion) {
+      case "+" : {
+        resultado = operando1 + operando2;
+        break;
+      }
+      case "-" : {
+        resultado = operando1 - operando2;
+        break;
+      }
+      case "*" : {
+        resultado = operando1 * operando2;
+        break;
+      }
+      case "/" : {
+        resultado = operando1 / operando2;
+        break;
+      }
+      default:
+      text ="Tenés que elegir una operación"
     }
-    case "*" : {
-      resultado = operando1 * operando2;
-      break;
-    }
-    case "/" : {
-      resultado = operando1 / operando2;
-      break;
-    }
-    default:
-    text ="Tenés que elegir una operación"
-  }
-  resultado = resultado.toFixed(2);
-  document.getElementById('resultado').innerHTML = resultado;
- });
+    resultado = resultado.toFixed(2);
+    document.getElementById('resultado').innerHTML = resultado;
+  });
 
 
-//Resetea todo con click en el botón 'reset'
+  //Resetea todo con click en el botón 'reset'
   $('#reset').click(function() {
     $('input:text').val('');
     document.getElementById('resultado').innerHTML = '';
-      var radioChecked = document.querySelector('input[name="operacion"]:checked');
-      radioChecked.parentElement.style.backgroundColor = "rgba(181, 254, 55, 0.46)"
- });
-
-
+    var radioChecked = document.querySelectorAll('input[name="operacion"]');
+    radioChecked.forEach(function (item, index, arr) {
+      arr[index].parentElement.style.backgroundColor = "rgb(143, 242, 7)";
+    });
+  });
 
 });
